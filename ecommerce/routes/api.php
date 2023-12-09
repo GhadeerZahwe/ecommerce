1e8606e67fe212fc26ee55006427285aaba03b6d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-10 00:26:46 +0200

    fix get products

--- ecommerce/routes/api.php
+++ ecommerce/routes/api.php
@@ -11,7 +11,7 @@ Route::post('refresh', [AuthController::class, 'refresh']);
 
 
 // Product routes
-Route::get('/products', [ProductController::class, 'getProducts']);
+Route::get('/products', [ProductController::class, 'get_products']);
 Route::post('/products', [ProductController::class, 'addProduct']);
 Route::post('/products/update/{id}', [ProductController::class, 'updateProduct']);
 Route::delete('/products/{id}', [ProductController::class, 'deleteProduct']);
