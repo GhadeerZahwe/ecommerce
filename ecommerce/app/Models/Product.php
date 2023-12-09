635f24ef148b4b8d42afe920d564dc79abb139a2
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 22:40:40 +0200

    add product model

--- ecommerce/app/Models/Product.php
+++ ecommerce/app/Models/Product.php
@@ -5,7 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Transaction extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ["user_id", "product_id", "quantity", "total_price"];
+
+    /**
+     * Define a relationship: a transaction belongs to a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Define a relationship: a transaction belongs to a product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        // The second parameter specifies the foreign key (product_id) on the transactions table.
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
