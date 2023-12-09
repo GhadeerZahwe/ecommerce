12e35be981c77687e01c4c799b1ae9dd9a85cef9
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 22:49:22 +0200

    add register

--- ecommerce/app/Http/Controllers/AuthController.php
+++ ecommerce/app/Http/Controllers/AuthController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'user_role' => 'required|integer',
+            'gender' => 'required|boolean'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_role' => $request->user_role,
+            'gender' => $request->gender
+        ]);
+
+        $token = Auth::login($user);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
 }
