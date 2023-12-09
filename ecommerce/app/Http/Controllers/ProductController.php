1cdeb636adb17f4e93ebdcf2a645ee85e8c0ffa4
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-10 00:15:35 +0200

    import models to product controller

--- ecommerce/app/Http/Controllers/ProductController.php
+++ ecommerce/app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 }
