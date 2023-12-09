2434a277e34f2b30b651cc4cc61745ed605c5038
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 22:50:24 +0200

    add login

--- ecommerce/app/Http/Controllers/AuthController.php
+++ ecommerce/app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
     public function register(Request $request)
     {
         $request->validate([
@@ -38,4 +43,32 @@ class AuthController extends Controller
             ]
         ]);
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
 }
