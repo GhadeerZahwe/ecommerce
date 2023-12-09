4db7a1d97ad03f425b96f871834d3ebeb6db9915
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-10 00:20:19 +0200

    crud operations for products

--- ecommerce/app/Http/Controllers/ProductController.php
+++ ecommerce/app/Http/Controllers/ProductController.php
@@ -7,8 +7,104 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * Constructor method to set middleware for authentication.
+     */
     public function __construct()
     {
         $this->middleware('auth:api');
+    }
+
+    /**
+     * Add a new product.
+     */
+    public function add_product(Request $request)
+    {
+        $user_role = auth()->user()->user_role;
+
+        // If user is a seller
+        if ($user_role == 1) {
+            // Merge the user_id from the authenticated user into the request
+            $user_id = ["user_id" => auth()->user()->id];
+            $request->merge($user_id);
+
+            // Create a new product
+            $product = Product::create($request->all());
+
+            return response()->json([
+                "status" => "success",
+                "data" => $product
+            ]);
+        }
+    }
+
+    /**
+     * Get all products.
+     */
+    public function get_products()
+    {
+        // Get all products
+        return response()->json([
+            "status" => "success",
+            "data" => Product::all()
+        ]);
+    }
+
+    /**
+     * Delete a product.
+     */
+    public function delete_product(Request $request, $id)
+    {
+        $user_role = auth()->user()->user_role;
+
+        // If user is a seller
+        if ($user_role == 1) {
+            // Find the product by ID
+            $product = Product::find($id);
+
+            if (!$product) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "Product not found"
+                ]);
+            }
+
+            // Delete the product
+            $product->delete();
+
+            return response()->json([
+                "status" => "success",
+                "message" => "Product deleted"
+            ]);
+        }
+    }
+
+    /**
+     * Update a product.
+     */
+    public function update_product(Request $request, $id)
+    {
+        $user_role = auth()->user()->user_role;
+
+        // If user is a seller
+        if ($user_role == 1) {
+            // Find the product by ID
+            $product = Product::find($id);
+
+            if (!$product) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "Product not found"
+                ]);
+            }
+
+            // Update the product
+            $product->update($request->all());
+
+            return response()->json([
+                "status" => "success",
+                "data" => $product
+            ]);
+        }
     }
 }
--- ecommerce/routes/api.php
+++ ecommerce/routes/api.php
@@ -8,4 +8,12 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout']);
 Route::post('refresh', [AuthController::class, 'refresh']);
+
+
+// Product routes
+Route::get('/products', [ProductController::class, 'getProducts']);
+Route::post('/products', [ProductController::class, 'addProduct']);
+Route::post('/products/update/{id}', [ProductController::class, 'updateProduct']);
+Route::delete('/products/{id}', [ProductController::class, 'deleteProduct']);
 ?>
+
