0c7fd2e61a0bd96ecf3f057580b12afbe49bb32c
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 22:08:37 +0200

    update products table

--- ecommerce/database/migrations/2023_12_09_200628_create-products_table.php
+++ ecommerce/database/migrations/2023_12_09_200628_create-products_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->integer('quantity');
-            $table->integer('total_price');
+            $table->string('product_name');
+            $table->string('product_description');
+            $table->integer('product_price');
+            $table->integer('stock_quantity');
             $table->integer('user_id');
-            $table->integer('product_id');
             $table->timestamps();
          });
     }
